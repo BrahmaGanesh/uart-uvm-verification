e634e5a8513a5afcfbe8a8d83a602a1fa408f091
BrahmaGanesh <katrapallibrahmaganesh@gmail.com>
2025-12-30 22:42:26 +0530

    uart_back_to_back_seq: add back-to-back transaction test

--- tb/sequences/uart_back_to_back_seq.sv
+++ tb/sequences/uart_back_to_back_seq.sv
@@ -2,7 +2,7 @@
 // Project     : UART_UVM_VERIFICATION
 // File        : uart_back_to_back_seq.sv
 // Author      : Brahma Ganesh Katrapalli
-// Date        : 27-12-2025
+// Date        : 30-12-2025
 // Version     : 1.0
 // Description : UART UVM sequence/test for back-to-back
 //               transactions with fixed clk_per_bit.
